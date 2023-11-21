3259b7308bdff989be7e94225b9feb2344516393
[Mac] euihyunee <lobil7782@gmail.com>
2023-11-21 23:17:02 +0900

    [UPDATE] img 경로 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700575877';
+const cacheName = 'chirpy-1700576086';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
