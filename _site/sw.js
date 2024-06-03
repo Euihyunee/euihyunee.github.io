48387befc809452cddecc556947e06eec44aa4e8
euihyunee <lobil7782@gmail.com>
2024-06-03 13:18:16 +0900

    upload profile

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240603.124435';
+const cacheName = 'chirpy-20240603.131706';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
