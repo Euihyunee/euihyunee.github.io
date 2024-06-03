ee69de8b31e29a42a8ecb006d8f1ef0ff9a8464a
euihyunee <lobil7782@gmail.com>
2024-06-03 12:45:13 +0900

    feat recent-list custom

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240603.123524';
+const cacheName = 'chirpy-20240603.124435';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
