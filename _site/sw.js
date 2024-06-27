9c24d39216ed7b23032819e1e9d663414ca1ca47
euihyunee <lobil7782@gmail.com>
2024-06-27 18:50:33 +0900

    [fix] chirpy 작성법 코드블럭 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240627.180735';
+const cacheName = 'chirpy-20240627.185002';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
